057680bffad539f62a8fc96d93fa3056025cd8d3
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-06-23 05:35:36 +0300

    Project Updated

--- forms-actions-no2/src/components/NewOpinion.jsx
+++ forms-actions-no2/src/components/NewOpinion.jsx
@@ -1,28 +1,105 @@
+import { useActionState } from "react";
+
+async function sendDataToBackEnd(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("There is error while uploading Post. Try Again!");
+  }
+  return true;
+}
+
 export function NewOpinion() {
+  function handleSubmit(prevFormState, formData) {
+    const userName = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    let errors = [];
+    if (!userName.trim()) {
+      errors.push("Please enter a valid username!");
+    }
+    if (!title.trim()) {
+      errors.push("Please enter a valid post title!");
+    }
+    if (!body.trim()) {
+      errors.push("Please enter a valid post body!");
+    }
+    if (errors.length > 0) {
+      return {
+        errors,
+        entredValues: {
+          userName,
+          title,
+          body,
+        },
+      };
+    }
+    sendDataToBackEnd("http://localhost:3000/opinions", {
+      userName,
+      title,
+      body,
+    });
+    return {
+      errors: null,
+    };
+  }
+  const [formState, formAction] = useActionState(handleSubmit, {
+    errors: null,
+  });
+
+  console.log(formState);
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.entredValues?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={formState.entredValues?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.entredValues?.body}
+          ></textarea>
         </p>
 
         <p className="actions">
           <button type="submit">Submit</button>
         </p>
       </form>
+      {formState.errors && (
+        <ul>
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
